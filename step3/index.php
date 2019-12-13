4719888417cfa9ad670f2ab01a1ae8207632c6a7
MATVEN <mattiavena2993@gmail.com>
2019-12-13 12:27:30 +0100

    setting step 3

---
+++ step3/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <div class="container">
+
+      <div class="box">
+        <div class="chart-line">
+
+        </div>
+      </div>
+
+      <div class="box">
+        <div class="chart-pie">
+
+        </div>
+      </div>
+
+      <div class="box">
+        <div class="chart-team">
+
+        </div>
+      </div>
+
+    </div>
+  </body>
+</html>
