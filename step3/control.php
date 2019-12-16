97c567195675f7f6d79d868ef59808a14c0c3e62
MATVEN <mattiavena2993@gmail.com>
2019-12-16 09:32:01 +0100

    add control step 3

--- step3/control.php
+++ step3/control.php
@@ -5,27 +5,34 @@ include 'data.php';
 // lettura parametro Get
 
 foreach ($graphs as $key => $value) {
-  
+
   unset($graphs[$key]);
 }
 
  ?>
 
 
-<?php /* <?php
+<?php
   include "data.php";
   $access = $_GET["level"];
 
   foreach ($graphs as $key => $value) {
 
-    if($access=="employee" && $value["access"]=="clevel"){
-      unset($graphs[$key]);
-    } else if($access=="guest"){
-      if( $value["access"]=="clevel"){
-        unset($graphs[$key]);
-      } elseif ($value["access"]=="employee") {
-        unset($graphs[$key]);
-      }
+    if($access == "guest"{
+      echo $graphs['fatturato']);
+
+    }if($access == "employee"){
+      echo $graphs['fatturato'])
+      .$graphs['fatturato_by_agent']);
+
+    }elseif ($access == "clevel") {
+      echo $graphs;
     }
   }
  ?>
+
+ if( $value["access"]=="clevel"){
+   unset($graphs[$key]);
+ } elseif ($value["access"]=="employee") {
+   unset($graphs[$key]);
+ }
